9b09e7593a61a1d1703ca149c133f326bdc9ac2b
Taesung Choi <taesung1031@gmail.com>
2024-09-24 19:33:43 +0900

    Update settings.py

--- ants_final/ants_final/settings.py
+++ ants_final/ants_final/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 # load_dotenv('/home/ants/django/.env')
-load_dotenv('../.env') # 윈도우에서 작업시 사용
+# load_dotenv('../.env') # 윈도우에서 작업시 사용
+load_dotenv('/home/runner/work/django/django/.env')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
